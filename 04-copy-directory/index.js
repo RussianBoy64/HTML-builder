4d361034c973c96418534ee514b07d21f7cbbd64
RussianBoy64 <kozlachkovvd@gmail.com>
2022-11-08 19:16:25 +0400

    feat: removed files from origin folder removes from copy folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,7 +1,7 @@
 const { stdout } = process
 const path = require('path')
 const fs = require('fs')
-const { mkdir, copyFile } = require('fs/promises')
+const { mkdir, rm, copyFile } = require('fs/promises')
 
 const originalPath = path.join(__dirname, 'files')
 const copyPath = path.join(__dirname, 'files-copy')
@@ -9,6 +9,8 @@ const copyPath = path.join(__dirname, 'files-copy')
 const readOriginFolder = async (err, files) => {
   if (err) stdout.write(err.message)
 
+  // remove folder if it exists
+  await rm(copyPath, { recursive: true })
   // create copy folder
   await mkdir(copyPath, { recursive: true })
 
