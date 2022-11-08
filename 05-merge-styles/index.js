0ed5b12a41037c398bd47c74e8eb067601f6cade
RussianBoy64 <kozlachkovvd@gmail.com>
2022-11-08 23:04:36 +0400

    feat: add slution for 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,35 @@
+const { stdout } = process
+const path = require('path')
+const fs = require('fs')
+
+const stylesPath = path.join(__dirname, 'styles')
+const bundlePath = path.join(__dirname, 'project-dist', 'bundle.css')
+
+const readStylesFolder = (err, files) => {
+  if (err) stdout.write(err.message)
+
+  const writeStream = fs.createWriteStream(bundlePath)
+
+  files.forEach((file) => {
+    // check is file
+    const fileExt = path.extname(file.name)
+
+    // read only css files
+    if (fileExt === '.css') {
+      //create path to each file
+      const filePath = path.join(stylesPath, file.name)
+
+      const readStream = fs.createReadStream(filePath, 'utf-8')
+
+      readStream.on('data', (chunk) => writeStream.write(chunk))
+
+      readStream.on('data', (chunk) => {
+        writeStream.write(chunk)
+      })
+
+      // readStream.pipe(writeStream)
+    }
+  })
+}
+
+fs.readdir(stylesPath, { withFileTypes: true }, readStylesFolder)
